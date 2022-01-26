264f038881365a97a09e81dc21b1eafb1d830489
HolyMongo <pererikviktor@gmail.com>
2022-01-26 10:02:50 +0100

    Buttons
    I made it so that the buttons change background when you enter and leave your pointer over then

--- GameProhect/Form1.Designer.cs
+++ GameProhect/Form1.Designer.cs
@@ -28,34 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Background = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.Background)).BeginInit();
+            this.start2DPlatformer = new System.Windows.Forms.Button();
+            this.nameOfTheGame = new System.Windows.Forms.Label();
+            this.Start3IRad = new System.Windows.Forms.Button();
+            this.StartPizzaMedKebab = new System.Windows.Forms.Button();
+            this.StartSnake = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Background
+            // start2DPlatformer
             // 
-            this.Background.Location = new System.Drawing.Point(100, 61);
-            this.Background.Name = "Background";
-            this.Background.Size = new System.Drawing.Size(480, 152);
-            this.Background.TabIndex = 0;
-            this.Background.TabStop = false;
+            this.start2DPlatformer.Location = new System.Drawing.Point(39, 280);
+            this.start2DPlatformer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.start2DPlatformer.Name = "start2DPlatformer";
+            this.start2DPlatformer.Size = new System.Drawing.Size(117, 63);
+            this.start2DPlatformer.TabIndex = 0;
+            this.start2DPlatformer.Text = "2D Platformer";
+            this.start2DPlatformer.UseVisualStyleBackColor = true;
+            this.start2DPlatformer.MouseEnter += new System.EventHandler(this.start2DPlatformer_MouseEnter);
+            this.start2DPlatformer.MouseLeave += new System.EventHandler(this.start2DPlatformer_MouseLeave);
+            // 
+            // nameOfTheGame
+            // 
+            this.nameOfTheGame.AutoSize = true;
+            this.nameOfTheGame.Font = new System.Drawing.Font("Comic Sans MS", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameOfTheGame.Location = new System.Drawing.Point(152, 25);
+            this.nameOfTheGame.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nameOfTheGame.Name = "nameOfTheGame";
+            this.nameOfTheGame.Size = new System.Drawing.Size(193, 84);
+            this.nameOfTheGame.TabIndex = 1;
+            this.nameOfTheGame.Text = "Start";
+            // 
+            // Start3IRad
+            // 
+            this.Start3IRad.Location = new System.Drawing.Point(294, 280);
+            this.Start3IRad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Start3IRad.Name = "Start3IRad";
+            this.Start3IRad.Size = new System.Drawing.Size(117, 63);
+            this.Start3IRad.TabIndex = 2;
+            this.Start3IRad.Text = "3 I Rad";
+            this.Start3IRad.UseVisualStyleBackColor = true;
+            this.Start3IRad.MouseEnter += new System.EventHandler(this.Start3IRad_MouseEnter);
+            this.Start3IRad.MouseLeave += new System.EventHandler(this.Start3IRad_MouseLeave);
+            // 
+            // StartPizzaMedKebab
+            // 
+            this.StartPizzaMedKebab.Location = new System.Drawing.Point(294, 492);
+            this.StartPizzaMedKebab.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.StartPizzaMedKebab.Name = "StartPizzaMedKebab";
+            this.StartPizzaMedKebab.Size = new System.Drawing.Size(117, 63);
+            this.StartPizzaMedKebab.TabIndex = 4;
+            this.StartPizzaMedKebab.Text = "Nils special";
+            this.StartPizzaMedKebab.UseVisualStyleBackColor = true;
+            this.StartPizzaMedKebab.MouseEnter += new System.EventHandler(this.StartPizzaMedKebab_MouseEnter);
+            this.StartPizzaMedKebab.MouseLeave += new System.EventHandler(this.StartPizzaMedKebab_MouseLeave);
+            // 
+            // StartSnake
+            // 
+            this.StartSnake.Location = new System.Drawing.Point(39, 492);
+            this.StartSnake.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.StartSnake.Name = "StartSnake";
+            this.StartSnake.Size = new System.Drawing.Size(117, 63);
+            this.StartSnake.TabIndex = 3;
+            this.StartSnake.Text = "Snake";
+            this.StartSnake.UseVisualStyleBackColor = true;
+            this.StartSnake.MouseEnter += new System.EventHandler(this.StartSnake_MouseEnter);
+            this.StartSnake.MouseLeave += new System.EventHandler(this.StartSnake_MouseLeave);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Background);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(478, 645);
+            this.Controls.Add(this.StartPizzaMedKebab);
+            this.Controls.Add(this.StartSnake);
+            this.Controls.Add(this.Start3IRad);
+            this.Controls.Add(this.nameOfTheGame);
+            this.Controls.Add(this.start2DPlatformer);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.Background)).EndInit();
+            this.Text = "Start";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox Background;
+        private System.Windows.Forms.Button start2DPlatformer;
+        private System.Windows.Forms.Label nameOfTheGame;
+        private System.Windows.Forms.Button Start3IRad;
+        private System.Windows.Forms.Button StartPizzaMedKebab;
+        private System.Windows.Forms.Button StartSnake;
     }
 }
 
--- GameProhect/Form1.cs
+++ GameProhect/Form1.cs
@@ -8,15 +8,95 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
+/*
+ 
+Byt Nins special mot en kibabpizza
+
+när man trycker på den så kommer det en kibabpizza
+
+
+ 
+ */
+
+
 namespace GameProhect
 {
     public partial class Form1 : Form
     {
+
+        int[] gameSelector = new int[3] {0, 1, 2};
+        int gamePage;
         public Form1()
         {
             InitializeComponent();
+            
+
         }
 
-        
+		private void Background_Click(object sender, EventArgs e)
+		{
+
+		}
+
+        private void positiveSlide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void start2DPlatformer_MouseEnter(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(0, 200, 0);
+            nameOfTheGame.Text = "2D Platformer";
+            nameOfTheGame.Location = new Point(nameOfTheGame.Location.X - 86, nameOfTheGame.Location.Y);
+        }
+
+        private void start2DPlatformer_MouseLeave(object sender, EventArgs e)
+        {
+            BackColor = Color.LightBlue;
+            nameOfTheGame.Text = "Start";
+            nameOfTheGame.Location = new Point(nameOfTheGame.Location.X + 86, nameOfTheGame.Location.Y);
+        }
+
+        private void Start3IRad_MouseEnter(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(200, 0, 0);
+            nameOfTheGame.Text = "3 I Rad";
+            nameOfTheGame.Location = new Point(nameOfTheGame.Location.X - 20, nameOfTheGame.Location.Y);
+        }
+
+        private void Start3IRad_MouseLeave(object sender, EventArgs e)
+        {
+            BackColor = Color.LightBlue;
+            nameOfTheGame.Text = "Start";
+            nameOfTheGame.Location = new Point(nameOfTheGame.Location.X + 20, nameOfTheGame.Location.Y);
+        }
+
+        private void StartSnake_MouseEnter(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(0, 0, 200);
+            nameOfTheGame.Text = "Snake";
+        }
+
+        private void StartSnake_MouseLeave(object sender, EventArgs e)
+        {
+            BackColor = Color.LightBlue;
+            nameOfTheGame.Text = "Start";
+        }
+
+        private void StartPizzaMedKebab_MouseEnter(object sender, EventArgs e)
+        {
+            BackColor = Color.Purple;
+            nameOfTheGame.Text = "Kebabbpizza";
+            nameOfTheGame.Location = new Point(nameOfTheGame.Location.X - 65, nameOfTheGame.Location.Y);
+        }
+
+        private void StartPizzaMedKebab_MouseLeave(object sender, EventArgs e)
+        {
+            BackColor = Color.LightBlue;
+            nameOfTheGame.Text = "Start";
+            nameOfTheGame.Location = new Point(nameOfTheGame.Location.X + 65, nameOfTheGame.Location.Y);
+        }
     }
 }
